35a3d015eeca5313d0f51300ca0785ca6913f18a
Allison <andersonaallison@gmail.com>
2019-05-15 22:05:58 -0600

    Updated Spotify data for song pulll, deleted main.js and consolidated into liri.js, added support for commands and search terms.

--- liri.js
+++ liri.js
@@ -1,60 +1,93 @@
+/*jshint esversion: 6 */
 require("dotenv").config();
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-node liri.js movie-this '<movie name here>'
+const logInfo = (info) => {
+    console.log('\x1b[33m%s\x1b[0m', "Info: ", info);
+};
 
-// What Each Command Should Do
+const logError = (err) => {
+    console.log('\x1b[31m%s\x1b[0m', "Error: ", err);
+};
 
-// node liri.js concert-this <artist/band name here>
+const print = (str) => {
+    console.log('\x1b[36m%s\x1b[0m', str);
+};
 
-// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+const handleConcert = (searchTerm) => {
+    logInfo(`Handling concert with "${searchTerm}"`);
+};
 
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
+const printSpotify = (song) => {
+    print(`Artist(s): ${song.artists[0].name}`);
+    print(`Song: ${song.name}`);
+    print(`Preview: ${song.external_urls.spotify}`);
+    print(`Album: ${song.album.name}`);
+};
 
-// node liri.js spotify-this-song '<song name here>'
+const handleSpotify = (searchTerm) => {
+    logInfo(`Handling Spotify with "${searchTerm}"`);
+    spotify.search({
+        type: 'track',
+        query: searchTerm,
+    }, (err, data) => {
+        if (err) {
+            logError(err);
+            return;
+        }
 
-// This will show the following information about the song in your terminal/bash window
+        printSpotify(data.tracks.items[0]);
+    });
+};
 
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
+const handleMovie = (searchTerm) => {
+    logInfo(`Handling movie with "${searchTerm}"`);
 
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
+};
 
+const handleDoIt = (searchTerm) => {
+    logInfo(`Handling do it with "${searchTerm}"`);
+
+};
 // node liri.js movie-this '<movie name here>'
+const commands = {
+    "concert-this": handleConcert,
+    "spotify-this-song": handleSpotify,
+    "movie-this": handleMovie,
+    "do-what-it-says": handleDoIt,
+};
 
-// This will output the following information to your terminal/bash window:
+const handleCommand = (command, searchTerm) => {
+    if (command in commands) {
+        commands[command](searchTerm);
+    } else {
+        logError(`Command: ${command} not available.`);
+    }
+}
 
-//    * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Rotten Tomatoes Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
+const parseCommand = (argv) => {
+    const command = argv[0];
+    const searchTerm = argv.slice(1).join(" ");
 
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    return {
+        command,
+        searchTerm,
+    };
+};
 
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+const main = (argv) => {
+    if (argv.length === 0) {
+        logError("No command provided");
+        return;
+    }
+    if (argv.length < 2) {
+        logError("No search term provided");
+        return;
+    }
+    const input = parseCommand(argv);
+    handleCommand(input.command, input.searchTerm);
+};
 
-// It's on Netflix!
-
-// You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
-
-// node liri.js do-what-it-says
-
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Edit the text in random.txt to test out the feature for movie-this and concert-this.
+main(process.argv.slice(2));
--- main.js
+++
@@ -1,5 +0,0 @@
-let main = (argv) => {
-    console.log(`${argv}`);
-}
-
-main(process.argv.slice(2));
